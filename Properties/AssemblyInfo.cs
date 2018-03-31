dccf2201dbdaae03be50b574d626811db1315da0
Dmitry Brant <dbrant@wikimedia.org>
2018-03-31 13:51:35 -0400

    Bump version number.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ThumbCacheViewer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tool for examining Windows Explorer thumbnail caches.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Defiant Technologies, LLC.")]
 [assembly: AssemblyProduct("ThumbCacheViewer")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.37")]
+[assembly: AssemblyFileVersion("1.0.0.37")]
