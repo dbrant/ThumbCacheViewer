a09484be49659774ce1c8ea68d39c252fa6c6a48
Dmitry Brant <dbrant@wikimedia.org>
2021-09-18 15:07:22 -0400

    Bump version number.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Defiant Technologies, LLC.")]
 [assembly: AssemblyProduct("ThumbCacheViewer")]
-[assembly: AssemblyCopyright("Copyright © Dmitry Brant 2018-2020")]
+[assembly: AssemblyCopyright("Copyright © Dmitry Brant 2018-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.37")]
-[assembly: AssemblyFileVersion("1.0.0.37")]
+[assembly: AssemblyVersion("1.0.0.43")]
+[assembly: AssemblyFileVersion("1.0.0.43")]
