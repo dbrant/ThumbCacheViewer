abc206e4dcf7d5b32db8b07e87009aeb7d0db97d
Dmitry Brant <me@dmitrybrant.com>
2022-12-04 09:21:36 -0500

    Upgrade to VS 2022 / .NET 6.

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace ThumbCacheViewer
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
