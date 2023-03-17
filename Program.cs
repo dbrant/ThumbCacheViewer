ce9c43d638a738c2b7d6d346775a304f3296907f
Dmitry Brant <me@dmitrybrant.com>
2023-03-17 08:16:20 -0400

    Update high dpi mode.

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace ThumbCacheViewer
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
